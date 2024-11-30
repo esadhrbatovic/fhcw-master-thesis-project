82bad34111604480c6c5cee6f4eb15a1e147f637
Esad Hrbatovic <esadh@live.de>
2024-11-30 21:04:22 +0100

    order with mail notification

--- prototype/quarkus/eh-ma-quarkus-notificationservice/src/main/java/com/hrbatovic/quarkus/master/notification/HealthResource.java
+++ prototype/quarkus/eh-ma-quarkus-notificationservice/src/main/java/com/hrbatovic/quarkus/master/notification/HealthResource.java
@@ -1,5 +1,8 @@
 package com.hrbatovic.quarkus.master.notification;
 
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -9,10 +12,14 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/health")
 public class HealthResource {
 
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String health() throws Exception {
+
         return "Quarkus notificationservice is up and running!";
     }
+
+
 
 }
--- prototype/quarkus/eh-ma-quarkus-notificationservice/src/main/java/com/hrbatovic/quarkus/master/notification/messaging/consumers/MessageConsumer.java
+++ prototype/quarkus/eh-ma-quarkus-notificationservice/src/main/java/com/hrbatovic/quarkus/master/notification/messaging/consumers/MessageConsumer.java
@@ -2,9 +2,12 @@ package com.hrbatovic.quarkus.master.notification.messaging.consumers;
 
 import com.hrbatovic.quarkus.master.notification.messaging.model.LicensesGeneratedEventPayload;
 import com.hrbatovic.quarkus.master.notification.messaging.model.OrderNotificationSentEventPayload;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import org.eclipse.microprofile.context.ManagedExecutor;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -13,20 +16,31 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 public class MessageConsumer {
 
     @Inject
+    Mailer mailer;
+
+    @Inject
+    ManagedExecutor executor;
+
+    @Inject
     @Channel("order-notification-sent-out")
     Emitter<OrderNotificationSentEventPayload> orderNotificationSentEmitter;
 
     @Incoming("licenses-generated-in")
     public void onLicensesGenerated(LicensesGeneratedEventPayload licensesGeneratedEventPayload){
         System.out.println("Recieved licenses-generated-in event: " + licensesGeneratedEventPayload);
+        executor.runAsync(() -> {
+            sendOrderConfirmation(licensesGeneratedEventPayload);
+            OrderNotificationSentEventPayload orderNotificationSentEventPayload = new OrderNotificationSentEventPayload();
+            orderNotificationSentEventPayload.setOrderId(licensesGeneratedEventPayload.getOrderId());
+            orderNotificationSentEmitter.send(orderNotificationSentEventPayload);
+        });
 
-        //sendOrderConfirmation(licensesGeneratedEventPayload);
-
-        OrderNotificationSentEventPayload orderNotificationSentEventPayload = new OrderNotificationSentEventPayload();
-        orderNotificationSentEventPayload.setOrderId(licensesGeneratedEventPayload.getOrderId());
-        orderNotificationSentEmitter.send(orderNotificationSentEventPayload);
     }
 
+
+    public void sendOrderConfirmation(LicensesGeneratedEventPayload licensesGeneratedEventPayload){
+        mailer.send(Mail.withText("esadh@live.de", "test esad mail", "Hi there! " + licensesGeneratedEventPayload.getLicenses()));
+    }
 
 
 }

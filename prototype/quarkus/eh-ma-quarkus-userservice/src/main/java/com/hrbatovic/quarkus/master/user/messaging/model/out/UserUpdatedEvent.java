d0eec2f7169e35ef632abcdf4a0c5560fd2d259c
Esad Hrbatovic <esadh@live.de>
2024-12-05 22:03:40 +0100

    auth, user, segregate db from events, mapstruct usage

---
+++ prototype/quarkus/eh-ma-quarkus-userservice/src/main/java/com/hrbatovic/quarkus/master/user/messaging/model/out/UserUpdatedEvent.java
@@ -0,0 +1,117 @@
+package com.hrbatovic.quarkus.master.user.messaging.model.out;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class UserUpdatedEvent implements Serializable {
+    public static class Address {
+        private String street;
+        private String city;
+        private String state;
+        private String postalCode;
+        private String country;
+
+        public Address() {
+        }
+
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        public String getPostalCode() {
+            return postalCode;
+        }
+
+        public void setPostalCode(String postalCode) {
+            this.postalCode = postalCode;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+    }
+
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String role;
+    private String phoneNumber;
+    private UserUpdatedEvent.Address address;
+
+    public UserUpdatedEvent() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public UserUpdatedEvent.Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(UserUpdatedEvent.Address address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}

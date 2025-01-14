9c8db54ccaa0cdf995e5b9198b255dac9b992668
Esad Hrbatovic <esadh@live.de>
2025-01-14 01:18:55 +0100

    spring boot projects created

---
+++ prototype/springboot/eh-ma-springboot-userservice/src/main/java/com/hrbat/master/springboot/HealthController.java
@@ -0,0 +1,14 @@
+package com.hrbat.master.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+	@GetMapping("/health")
+
+	public String getHealth() {
+		return "Spring-Boot is up and running!";
+	}
+}
